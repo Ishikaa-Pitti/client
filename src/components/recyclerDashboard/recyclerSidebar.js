5f7aac9d83ea757a4aa0d27f3a2cc7bf8ea9884a
Ishikaa-Pitti <codersayshello@gmail.com>
2023-04-13 04:03:55 +0530

    recycler dahboard sidebar added

---
+++ src/components/recyclerDashboard/recyclerSidebar.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { Drawer, ListItem } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ScheduleOutlinedIcon from '@mui/icons-material/ScheduleOutlined';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import PickupSchedule from '../clientDashboard/pickupSchedule';
+const drawerWidth = 220;
+const RecyclerSidebar = () => {
+    //const [isCollapsed, setIsCollapsed] = useState(false);
+    //const [selected, setSelected] = useState('Dashboard');
+    return (
+        <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+        <List>
+            <ListItem key="My Pickup" disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <ScheduleOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="My Orders" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem key="My Pickup" disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <PeopleAltOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Profile" />
+                </ListItemButton>
+            </ListItem>
+            
+        </List>
+        </Box>
+    </Drawer>
+    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar/>
+        <PickupSchedule />
+    </Box>
+    </Box>
+        
+        
+    )
+}
+export default RecyclerSidebar;
--- src/pages/dashboardr.js
+++ src/pages/dashboardr.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { ThemeProvider } from '@mui/material';
 import { theme } from '../App';
 import DashboardNavbar from '../components/clientDashboard/dashboardNavbar'
+import RecyclerSidebar from '../components/recyclerDashboard/recyclerSidebar';
 const Dashboardr = () => {
   return (
     <ThemeProvider theme={theme}>
       <DashboardNavbar />
+      <RecyclerSidebar/>
     </ThemeProvider>
   )
 }
